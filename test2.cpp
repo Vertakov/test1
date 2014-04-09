1e96efddfdf0917e9a403162f364593d621f862a
Petr Vertakov <vertakov@polymedia.ru>
2014-04-09 17:10:04 +0400

    добвление hello new visual studio

---
+++ test2.cpp
@@ -0,0 +1,13 @@
+// test2.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout<<"Hello new visual studio"<<endl;
+	return 0;
+}
+
